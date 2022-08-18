16576f710769bb68276ca5c1d2f281189f0b683f
pan <416500624@qq.com>
2022-08-18 13:32:27 +0800

    数据库字段自动加解密处理

--- mybatis-crypto-core/src/main/java/io/github/whitedg/mybatis/crypto/MybatisEncryptionPlugin.java
+++ mybatis-crypto-core/src/main/java/io/github/whitedg/mybatis/crypto/MybatisEncryptionPlugin.java
@@ -2,9 +2,13 @@ package io.github.whitedg.mybatis.crypto;
 
 import com.esotericsoftware.kryo.kryo5.Kryo;
 import org.apache.ibatis.binding.MapperMethod;
+import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +19,12 @@ import java.util.*;
  * @author White
  */
 @Intercepts({
-        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})
+        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class}),
+        @Signature(type = Executor.class, method = "query",
+                args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class,
+                        CacheKey.class, BoundSql.class}),
+        @Signature(type = Executor.class, method = "query",
+                args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})
 })
 public class MybatisEncryptionPlugin implements Interceptor {
 
